015a5a4df9923a7076585e8484727c1dfeae40c6
RuslanLytvynov <lytvynovrus@gmail.com>
2023-05-20 23:24:50 +0200

    Implemented functional from Lab-2 Following functionality is implemented: 1.	Stretching histogram 2.	Equalizing histogram by equalization 3.	Negation 4.	Reduction of gray levels by posterization 5.	Selective stretching; from the p1-p2 range to the q3-q4 range, especially when q3=0, q4=Lmax.

--- APO_Main.py
+++ APO_Main.py
@@ -109,6 +109,57 @@ class ImageWindow(QWidget):
         self.action_rgb_to_hsv.triggered.connect(self.convert_to_hsv)
         self.action_rgb_to_lab.triggered.connect(self.convert_to_lab)
 
+        # Adding Lab2 menu
+        self.menu_lab2 = QtWidgets.QMenu(self.menubar)
+        self.menu_lab2.setObjectName("menuLab2")
+        self.menu_lab2.setTitle("Lab2")
+
+        # Creating Stretch Histogram action
+        self.action_stretch_hist = QtWidgets.QAction(self)
+        self.action_stretch_hist.setText("Stretch Histogram")
+        self.action_stretch_hist.setStatusTip("Apply histogram stretching")
+        self.action_stretch_hist.setObjectName("actionStretchHist")
+
+        # Creating Equalize Histogram action
+        self.action_equalize_hist = QtWidgets.QAction(self)
+        self.action_equalize_hist.setText("Equalize Histogram")
+        self.action_equalize_hist.setStatusTip("Apply histogram equalization")
+        self.action_equalize_hist.setObjectName("actionEqualizeHist")
+
+        # Creating Negation action
+        self.action_negation = QtWidgets.QAction(self)
+        self.action_negation.setText("Negation")
+        self.action_negation.setStatusTip("Apply negation")
+        self.action_negation.setObjectName("actionNegation")
+
+        # Creating Posterization action
+        self.action_posterization = QtWidgets.QAction(self)
+        self.action_posterization.setText("Posterization")
+        self.action_posterization.setStatusTip("Apply posterization")
+        self.action_posterization.setObjectName("actionPosterization")
+
+        # Creating Selective Stretching action
+        self.action_selective_stretching = QtWidgets.QAction(self)
+        self.action_selective_stretching.setText("Selective Stretching")
+        self.action_selective_stretching.setStatusTip("Apply selective stretching")
+        self.action_selective_stretching.setObjectName("actionSelectiveStretching")
+
+        # Adding actions to Lab2 menu
+        self.menu_lab2.addAction(self.action_stretch_hist)
+        self.menu_lab2.addAction(self.action_equalize_hist)
+        self.menu_lab2.addAction(self.action_negation)
+        self.menu_lab2.addAction(self.action_posterization)
+        self.menu_lab2.addAction(self.action_selective_stretching)
+
+        # Adding menu to menubar
+        self.menubar.addAction(self.menu_lab2.menuAction())
+
+        # Connecting signals to actions
+        self.action_stretch_hist.triggered.connect(self.stretch_histogram)
+        self.action_equalize_hist.triggered.connect(self.equalize_histogram)
+        self.action_negation.triggered.connect(self.negation)
+        self.action_posterization.triggered.connect(self.posterization)
+        self.action_selective_stretching.triggered.connect(self.selective_stretching)
     def show_histogram(self):
         if self.is_gray:
             self.hist = self.build_histogram(self.image, 'black')
@@ -180,6 +231,90 @@ class ImageWindow(QWidget):
             qimage = QImage(rgb_image.data, w, h, w * ch, QImage.Format_RGB888)
         return qimage
 
+    def stretch_histogram(self):
+        if self.is_gray:
+            self.image = self.stretch_gray_histogram(self.image)
+            height, width = self.image.shape
+            bytes_per_line = width
+            q_img = QImage(self.image.data, width, height, bytes_per_line, QImage.Format_Indexed8)
+            self.label.setPixmap(QPixmap.fromImage(q_img))
+        else:
+            b, g, r = cv2.split(self.image)
+            b = self.stretch_gray_histogram(b)
+            g = self.stretch_gray_histogram(g)
+            r = self.stretch_gray_histogram(r)
+            self.image = cv2.merge((b, g, r))
+            height, width, _ = self.image.shape
+            bytes_per_line = width * 3
+            q_img = QImage(self.image.data, width, height, bytes_per_line, QImage.Format_RGB888)
+            self.label.setPixmap(QPixmap.fromImage(q_img))
+
+    def stretch_gray_histogram(self, img):
+        hist, bins = np.histogram(img.flatten(), 256, [0, 256])
+        cdf = hist.cumsum()
+        cdf_normalized = cdf * hist.max() / cdf.max()
+        cdf_m = np.ma.masked_equal(cdf, 0)
+        cdf_m = (cdf_m - cdf_m.min()) * 255 / (cdf_m.max() - cdf_m.min())
+        cdf = np.ma.filled(cdf_m, 0).astype('uint8')
+        img2 = cdf[img]
+        return img2
+
+    def equalize_histogram(self):
+        if self.is_gray:
+            self.image = self.equalize_gray_histogram(self.image)
+            height, width = self.image.shape
+            bytes_per_line = width
+            q_img = QImage(self.image.data, width, height, bytes_per_line, QImage.Format_Indexed8)
+            self.label.setPixmap(QPixmap.fromImage(q_img))
+        else:
+            b, g, r = cv2.split(self.image)
+            b = self.equalize_gray_histogram(b)
+            g = self.equalize_gray_histogram(g)
+            r = self.equalize_gray_histogram(r)
+            self.image = cv2.merge((b, g, r))
+            height, width, _ = self.image.shape
+            bytes_per_line = width * 3
+            q_img = QImage(self.image.data, width, height, bytes_per_line, QImage.Format_RGB888)
+            self.label.setPixmap(QPixmap.fromImage(q_img))
+
+    def equalize_gray_histogram(self, img):
+        equ = cv2.equalizeHist(img)
+        self.update_image()
+
+    def update_image(self):
+        # Convert the image data to a QImage object
+        q_img = self.convert_cv_to_qimage(self.image)
+        # Set the converted QImage as the pixmap for the QLabel widget
+        self.label.setPixmap(QPixmap.fromImage(q_img))
+
+    def negation(self):
+        self.image = 255 - self.image
+        self.update_image()
+
+    def posterization(self):
+        levels = 8  # Number of levels for posterization
+        self.image = np.floor_divide(self.image, 256 // levels) * (256 // levels)
+        self.update_image()
+
+    def selective_stretching(self):
+        p1 = 0  # Lower input range
+        p2 = 127  # Upper input range
+        q3 = 64  # Lower output range
+        q4 = 255  # Upper output range
+
+        if self.is_gray:
+            self.image = np.interp(self.image, [p1, p2], [q3, q4])
+        else:
+            h, w, _ = self.image.shape
+            r, g, b = cv2.split(self.image)
+            r = np.interp(r, [p1, p2], [q3, q4])
+            g = np.interp(g, [p1, p2], [q3, q4])
+            b = np.interp(b, [p1, p2], [q3, q4])
+            self.image = cv2.merge([r, g, b])
+
+        self.image = np.clip(self.image, 0, 255)
+        self.image = self.image.astype(np.uint8)
+        self.update_image()
 
 class UiMainWindow(QMainWindow):
     def __init__(self, parent=None):
@@ -190,22 +325,23 @@ class UiMainWindow(QMainWindow):
 
         self.windows = []
 
-        self.centralwidget = QtWidgets.QWidget(self)
-        self.centralwidget.setObjectName("centralwidget")
-        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.central_widget = QtWidgets.QWidget(self)
+        self.central_widget.setObjectName("centralWidget")
+
+        self.label = QtWidgets.QLabel(self.central_widget)
         self.label.setGeometry(QtCore.QRect(250, 0, 500, 60))
         self.label.setObjectName("label")
-        self.label.setText("Choose an image")
+        self.label.setText("Click 'File' -> 'Open' to choose an image.")
 
-        self.setCentralWidget(self.centralwidget)
+        self.setCentralWidget(self.central_widget)
 
         self.menubar = QtWidgets.QMenuBar(self)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 570, 30))
         self.menubar.setObjectName("menubar")
 
-        self.menu_file = QtWidgets.QMenu(self.menubar)
-        self.menu_file.setObjectName("menuFile")
-        self.menu_file.setTitle("File")
+        self.file_menu = QtWidgets.QMenu(self.menubar)
+        self.file_menu.setObjectName("fileMenu")
+        self.file_menu.setTitle("File")
 
         self.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(self)
@@ -228,11 +364,11 @@ class UiMainWindow(QMainWindow):
         self.action_exit.setShortcut("Ctrl+E")
         self.action_exit.setObjectName("actionExit")
 
-        self.menu_file.addAction(self.action_open)
-        self.menu_file.addSeparator()
-        self.menu_file.addAction(self.action_exit)
+        self.file_menu.addAction(self.action_open)
+        self.file_menu.addSeparator()
+        self.file_menu.addAction(self.action_exit)
 
-        self.menubar.addAction(self.menu_file.menuAction())
+        self.menubar.addAction(self.file_menu.menuAction())
 
         QtCore.QMetaObject.connectSlotsByName(self)
 
