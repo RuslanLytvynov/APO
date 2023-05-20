aafcf4cb9e85b07237e112448380752a7274474f
RuslanLytvynov <lytvynovrus@gmail.com>
2023-05-20 22:27:16 +0200

    Implemented functional from Lab-1
    Following functionality is implemented:
    1.  Loading a grayscale image, mandatory in BMP format; optionally in formats such as JPG, PNG, GIF - the use of libraries is allowed.
    2.  Loading a color image (formats as above) - the use of libraries is allowed.
    3.  Develop an algorithm for creating a histogram of the loaded grayscale images - custom code.
    4.  Implement histogram creation in the prepared program - graphical representation (for grayscale images).
    5.  Implement histogram creation in the prepared program - tabular representation (for grayscale images).
    6.  Histogram values updated "on the fly" - histogram values are updated when the image is modified.
    7.  Conversion of color images to grayscale - the use of libraries is allowed.
    8.  Separation of color images into 3 grayscale channels - the use of libraries is allowed.
    9.  Conversion of RGB images to HSV and Lab (directly decomposed) - the use of libraries is allowed.
    10. Implementation of profile lines (for grayscale images) - the use of libraries is allowed.

--- APO_Main.py
+++ APO_Main.py
@@ -1,209 +1,263 @@
-from PyQt5 import QtWidgets, QtGui, QtCore
-import numpy as np
-import cv2
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 import matplotlib.pyplot as plt
-import sys
+import cv2
+import numpy as np
 
-class ImageViewer(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle('APO Ruslan Lytvynov : Image Project')
-        self.image = None
-        self.gray_image = None
-        self.hist = None
-        self.gray_hist = None
+
+class HistWindow(QWidget):
+    def __init__(self, name, is_gray, hist, b_hist, g_hist, r_hist):
+        super(HistWindow, self).__init__()
+        self.setWindowTitle("Table Histogram of " + name)
+        self.table_widget = QTableWidget()
+
+        if is_gray:
+            self.table_widget.setRowCount(2)
+            self.table_widget.setColumnCount(257)
+
+            self.table_widget.setItem(0, 0, QTableWidgetItem("PIXEL SHADE"))
+            self.table_widget.setItem(1, 0, QTableWidgetItem("NUMBER OF BLACK"))
+            for i in range(1, 257):
+                self.table_widget.setItem(0, i, QTableWidgetItem(str(i - 1)))
+                self.table_widget.setItem(1, i, QTableWidgetItem(str(hist[i - 1])))
+        else:
+            self.table_widget.setRowCount(4)
+            self.table_widget.setColumnCount(257)
+
+            self.table_widget.setItem(0, 0, QTableWidgetItem("PIXEL SHADE"))
+            self.table_widget.setItem(1, 0, QTableWidgetItem("RED"))
+            self.table_widget.setItem(2, 0, QTableWidgetItem("GREEN"))
+            self.table_widget.setItem(3, 0, QTableWidgetItem("BLUE"))
+            for i in range(1, 257):
+                self.table_widget.setItem(0, i, QTableWidgetItem(str(i - 1)))
+                self.table_widget.setItem(1, i, QTableWidgetItem(str(r_hist[i - 1])))
+                self.table_widget.setItem(2, i, QTableWidgetItem(str(g_hist[i - 1])))
+                self.table_widget.setItem(3, i, QTableWidgetItem(str(b_hist[i - 1])))
+
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.table_widget)
+        self.setLayout(self.layout)
+
+
+class ImageWindow(QWidget):
+    def __init__(self, fName):
+        super(ImageWindow, self).__init__()
+        self.name = fName.split("/")[-1]
+        self.fullName = fName
+        self.setWindowTitle(self.name)
+        self.image = cv2.imread(fName)
+        self.image = self.image.copy()
+        self.label = QLabel(self)
+        self.label.setPixmap(QPixmap(fName))
+        layout = QVBoxLayout()
+        layout.addWidget(self.label)
+        self.setLayout(layout)
+
+        if len(self.image.shape) == 2:
+            self.is_gray = True
+        else:
+            self.is_gray = False
+
+        self.windows = []
 
         self.menubar = QtWidgets.QMenuBar(self)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 570, 30))
         self.menubar.setObjectName("menubar")
 
-        self.menuLab1 = QtWidgets.QMenu(self.menubar)
-        self.menuLab1.setObjectName("menuLab1")
-        self.menuLab1.setTitle("Lab1")
+        # Creating Lab1 menu
+        self.menu_lab1 = QtWidgets.QMenu(self.menubar)
+        self.menu_lab1.setObjectName("menuLab1")
+        self.menu_lab1.setTitle("Lab1")
 
-        # Przyciski i etykiety
-        self.open_button = QtWidgets.QPushButton("Otwórz obraz")
-        self.gray_button = QtWidgets.QPushButton("Przekształć do obrazu szaroodcieniowego")
-        self.hist_button = QtWidgets.QPushButton("Histogram (grafika)")
-        self.hist_table_button = QtWidgets.QPushButton("Histogram (tabela)")
-        self.profile_button = QtWidgets.QPushButton("Linia profilu")
-        self.image_label = QtWidgets.QLabel(self)
-        self.gray_image_label = QtWidgets.QLabel(self)
-        self.hist_label = QtWidgets.QLabel(self)
-        self.gray_hist_label = QtWidgets.QLabel(self)
-        self.stretch_button = QtWidgets.QPushButton("Rozciągnij histogram")
-        self.equalize_button = QtWidgets.QPushButton("Wyrównaj histogram")
-        self.negate_button = QtWidgets.QPushButton("Negacja")
-        self.posterize_button = QtWidgets.QPushButton("Redukcja poziomów szarości")
-        self.selective_stretch_button = QtWidgets.QPushButton("Rozciąganie selektywne")
-       
-        # Ustawienie layoutu
-        layout = QtWidgets.QGridLayout()
-        layout.addWidget(self.open_button, 0, 0)
-        layout.addWidget(self.gray_button, 0, 1)
-        layout.addWidget(self.hist_button, 0, 2)
-        layout.addWidget(self.hist_table_button, 0, 3)
-        layout.addWidget(self.profile_button, 0, 4)
-        layout.addWidget(self.image_label, 1, 0, 5, 1)
-        layout.addWidget(self.gray_image_label, 1, 1, 5, 1)
-        layout.addWidget(self.hist_label, 1, 2, 2, 2)
-        layout.addWidget(self.gray_hist_label, 3, 2, 2, 2)
-        layout.addWidget(self.stretch_button, 0, 5)
-        layout.addWidget(self.equalize_button, 0, 6)
-        layout.addWidget(self.negate_button, 1, 5)
-        layout.addWidget(self.posterize_button, 1, 6)
-        layout.addWidget(self.selective_stretch_button, 1, 7)
-        self.setLayout(layout)
+        # Creating Hist action
+        self.action_hist = QtWidgets.QAction(self)
+        self.action_hist.setText("Histogram")
+        self.action_hist.setStatusTip("Draw a histogram")
+        self.action_hist.setObjectName("actionHist")
 
-        # Podpięcie przycisków do funkcji
-        self.open_button.clicked.connect(self.open_image)
-        self.gray_button.clicked.connect(self.convert_to_gray)
-        self.hist_button.clicked.connect(self.show_histogram)
-        self.hist_table_button.clicked.connect(self.show_histogram_table)
-        self.profile_button.clicked.connect(self.show_profile)
-        self.stretch_button.clicked.connect(self.histogram_stretching)
-        self.equalize_button.clicked.connect(self.histogram_equalization)
-        self.negate_button.clicked.connect(self.negate)
-        self.posterize_button.clicked.connect(self.posterize)
-        self.selective_stretch_button.clicked.connect(self.selective_stretch)
-    def open_image(self):
-        # Otwieranie obrazu z pliku
-        filename, _ = QtWidgets.QFileDialog.getOpenFileName(
-            self, "Otwórz obraz", "", "Obraz (*.bmp *.jpg *.png *.gif)"
-        )
-        if filename:
-            self.image = cv2.imread(filename, cv2.IMREAD_COLOR)
-            self.show_image(self.image)
+        # Creating ConvertGray action
+        self.action_convert_gray = QtWidgets.QAction(self)
+        self.action_convert_gray.setText("ConvertGray")
+        self.action_convert_gray.setStatusTip("Convert to Gray")
+        self.action_convert_gray.setObjectName("actionConvertGray")
 
-    def show_image(self, image):
-        # Wyświetlanie obrazu w etykiecie
-        height, width, channel = image.shape
-        bytes_per_line = 3 * width
-        q_image = QtGui.QImage(
-            image.data, width, height, bytes_per_line, QtGui.QImage.Format_RGB888
-        )
-        pixmap = QtGui.QPixmap.fromImage(q_image)
-        self.image_label.setPixmap(pixmap)
+        # Creating RGB to HSV action
+        self.action_rgb_to_hsv = QtWidgets.QAction(self)
+        self.action_rgb_to_hsv.setText("RGB to HSV")
+        self.action_rgb_to_hsv.setStatusTip("Convert RGB to HSV")
+        self.action_rgb_to_hsv.setObjectName("actionRGBtoHSV")
 
-    def convert_to_gray(self):
-        # Konwersja obrazu do szaroodcieniowego i wyświetlenie go w etykiecie
-        if self.image is not None:
-            self.gray_image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
-            self.show_gray_image(self.gray_image)
+        # Creating RGB to Lab action
+        self.action_rgb_to_lab = QtWidgets.QAction(self)
+        self.action_rgb_to_lab.setText("RGB to Lab")
+        self.action_rgb_to_lab.setStatusTip("Convert RGB to Lab")
+        self.action_rgb_to_lab.setObjectName("actionRGBtoLab")
 
-    def show_gray_image(self, gray_image):
-        # Wyświetlenie obrazu szaroodcieniowego w etykiecie
-        height, width = gray_image.shape
-        bytes_per_line = width
-        q_image = QtGui.QImage(
-            gray_image.data, width, height, bytes_per_line, QtGui.QImage.Format_Grayscale8
-        )
-        pixmap = QtGui.QPixmap.fromImage(q_image)
-        self.gray_image_label.setPixmap(pixmap)
+        # Adding actions to Lab1 menu
+        self.menu_lab1.addAction(self.action_hist)
+        self.menu_lab1.addAction(self.action_convert_gray)
+        self.menu_lab1.addAction(self.action_rgb_to_hsv)
+        self.menu_lab1.addAction(self.action_rgb_to_lab)
 
-    def calculate_histogram(self, image):
-        # Obliczenie histogramu obrazu szaroodcieniowego
-        if image is not None:
-            hist, _ = np.histogram(image.flatten(), 256, [0, 256])
-            return hist
+        # Adding menu to menubar
+        self.menubar.addAction(self.menu_lab1.menuAction())
+
+        # Connecting signals to actions
+        self.action_hist.triggered.connect(self.show_histogram)
+        self.action_convert_gray.triggered.connect(self.convert_to_gray)
+        self.action_rgb_to_hsv.triggered.connect(self.convert_to_hsv)
+        self.action_rgb_to_lab.triggered.connect(self.convert_to_lab)
 
     def show_histogram(self):
-        try:
-            # Wyświetlenie histogramu w postaci graficznej
-            if self.gray_image is not None:
-                self.hist = self.calculate_histogram(self.gray_image)
-                plt.hist(self.gray_image.flatten(), 256, [0, 256], color="r")
-                plt.xlim([0, 256])
-                plt.show()
-        except Exception as e:
-            print("Wystąpił błąd:", e)
+        if self.is_gray:
+            self.hist = self.build_histogram(self.image, 'black')
+            self.tabula(self.name, self.is_gray, self.hist, None, None, None)
+        else:
+            b, g, r = cv2.split(self.image)
+            self.b_hist = self.build_histogram(b, 'b')
+            self.g_hist = self.build_histogram(g, 'g')
+            self.r_hist = self.build_histogram(r, 'r')
+            self.tabula(self.name, self.is_gray, None, self.b_hist, self.g_hist, self.r_hist)
 
-    def show_histogram_table(self):
-        # Wyświetlenie histogramu w postaci tabelarycznej
-        if self.gray_image is not None:
-            self.gray_hist = self.calculate_histogram(self.gray_image)
-            dialog = QtWidgets.QDialog()
-            dialog.setWindowTitle("Histogram")
-            dialog.setMinimumSize(400, 300)
-            layout = QtWidgets.QVBoxLayout(dialog)
-            table = QtWidgets.QTableWidget()
-            table.setRowCount(len(self.gray_hist))
-            table.setColumnCount(2)
-            for i, val in enumerate(self.gray_hist):
-                table.setItem(i, 0, QtWidgets.QTableWidgetItem(str(i)))
-                table.setItem(i, 1, QtWidgets.QTableWidgetItem(str(val)))
-            table.setHorizontalHeaderLabels(["Wartość", "Liczba pikseli"])
-            layout.addWidget(table)
-            dialog.exec_()
+    def build_histogram(self, img, color):
+        my_hist = np.zeros(256)
+        img = img.ravel()
+        for i in img:
+            my_hist[i] += 1
+        a_list = list(range(0, 256))
+        plt.figure("Histogram of " + self.name)
+        plt.bar(a_list, my_hist, alpha=0.7, color=color)
+        plt.show(block=False)
+        return my_hist
 
-    def show_profile(self):
-        # Wyświetlenie linii profilu
-        if self.gray_image is not None:
-            x, y = self.get_profile_line()
-            plt.plot(x, y, color="r")
-            plt.xlim([0, self.gray_image.shape[1]])
-            plt.ylim([0, 256])
-            plt.show()
+    def tabula(self, name, is_gray, hist, b_hist, g_hist, r_hist):
+        tabl = HistWindow(name, is_gray, hist, b_hist, g_hist, r_hist)
+        self.windows.append(tabl)
+        tabl.show()
 
-    def get_profile_line(self):
-        # Pobranie linii profilu
-        x1, y1, x2, y2 = 0, self.gray_image.shape[0] // 2, self.gray_image.shape[1], self.gray_image.shape[0] // 2
-        line = cv2.line(np.zeros_like(self.gray_image), (x1, y1), (x2, y2), 255, 1)
-        profile = cv2.bitwise_and(self.gray_image, self.gray_image, mask=line)
-        x = np.arange(self.gray_image.shape[1])
-        y = np.sum(profile, axis=0)
-        return x, y
+    def convert_to_gray(self):
+        if self.is_gray:
+            QMessageBox.warning(self, "Warning", "The image is already in grayscale.")
+        else:
+            self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+            height, width = self.image.shape
+            bytes_per_line = width
+            q_img = QImage(self.image.data, width, height, bytes_per_line, QImage.Format_Indexed8)
+            self.label.setPixmap(QPixmap.fromImage(q_img))
+            self.is_gray = True
 
-    def histogram_stretching(self):
-        if self.gray_image is not None:
-            # Obliczenie minimalnej i maksymalnej wartości pikseli w obrazie szaroodcieniowym
-            min_val, max_val, _, _ = cv2.minMaxLoc(self.gray_image)
+    def convert_to_hsv(self):
+        if len(self.image.shape) == 2:
+            QMessageBox.warning(self, "Warning", "The image is already in grayscale. HSV conversion is not possible.")
+        else:
+            self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV)
+            height, width, _ = self.image.shape
+            bytes_per_line = width * 3
+            q_img = QImage(self.image.data, width, height, bytes_per_line, QImage.Format_RGB888)
+            self.label.setPixmap(QPixmap.fromImage(q_img))
+            self.is_gray = False
 
-            # Przeprowadzenie rozciągania histogramu
-            stretched_image = ((self.gray_image - min_val) / (max_val - min_val)) * 255
-            stretched_image = stretched_image.astype(np.uint8)
+    def convert_to_lab(self):
+        if len(self.image.shape) == 2:
+            QMessageBox.warning(self, "Warning", "The image is already in grayscale. Lab conversion is not possible.")
+        else:
+            self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2LAB)
+            height, width, _ = self.image.shape
+            bytes_per_line = width * 3
+            q_img = QImage(self.image.data, width, height, bytes_per_line, QImage.Format_RGB888)
+            self.label.setPixmap(QPixmap.fromImage(q_img))
+            self.is_gray = False
 
-            # Wyświetlenie obrazu po rozciągnięciu histogramu
-            self.show_gray_image(stretched_image)
-   
-    def histogram_equalization(self):
-        if self.gray_image is not None:
-            # Przeprowadzenie wyrównania histogramu przez equalizację
-            equalized_image = cv2.equalizeHist(self.gray_image)
+    def convert_cv_to_qimage(self, image):
+        if len(image.shape) == 2:
+            gray_image = image
+            h, w = gray_image.shape
+            qimage = QImage(gray_image.data, w, h, w, QImage.Format_Grayscale8)
+        else:
+            rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            h, w, ch = rgb_image.shape
+            qimage = QImage(rgb_image.data, w, h, w * ch, QImage.Format_RGB888)
+        return qimage
 
-            # Wyświetlenie obrazu po wyrównaniu histogramu
-            self.show_gray_image(equalized_image)
 
-    def negate(self):
-        if self.gray_image is not None:
-            # Negacja obrazu
-            negated_image = 255 - self.gray_image
+class UiMainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        super(UiMainWindow, self).__init__(parent)
+        self.setObjectName("MainWindow")
+        self.resize(640, 200)
+        self.setWindowTitle("APO Ruslan Lytvynov : Image Project")
 
-            # Wyświetlenie obrazu po negacji
-            self.show_gray_image(negated_image)
+        self.windows = []
 
-    def posterize(self):
-        if self.gray_image is not None:
-            # Przeprowadzenie posterizacji obrazu
-            posterized_image = np.where(self.gray_image < 64, 32, self.gray_image)
-            posterized_image = np.where(posterized_image < 128, 96, posterized_image)
-            posterized_image = np.where(posterized_image < 192, 160, posterized_image)
-            posterized_image = np.where(posterized_image < 256, 224, posterized_image)
+        self.centralwidget = QtWidgets.QWidget(self)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(250, 0, 500, 60))
+        self.label.setObjectName("label")
+        self.label.setText("Choose an image")
 
-            # Wyświetlenie obrazu po posterizacji
-            self.show_gray_image(posterized_image)
+        self.setCentralWidget(self.centralwidget)
 
-    def selective_stretch(self):
-        if self.gray_image is not None:
-            # Przeprowadzenie rozciągania histogramu
-            stretched_image = ((self.gray_image - 0) / (255 - 0)) * 255
-            stretched_image = stretched_image.astype(np.uint8)
+        self.menubar = QtWidgets.QMenuBar(self)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 570, 30))
+        self.menubar.setObjectName("menubar")
 
-            # Wyświetlenie obrazu po rozciągnięciu histogramu
-            self.show_gray_image(stretched_image)
+        self.menu_file = QtWidgets.QMenu(self.menubar)
+        self.menu_file.setObjectName("menuFile")
+        self.menu_file.setTitle("File")
+
+        self.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(self)
+        self.statusbar.setObjectName("statusbar")
+        self.setStatusBar(self.statusbar)
+
+        self.action_open = QtWidgets.QAction(self)
+        self.action_open.setText("Open")
+        self.action_open.setIconText("Open")
+        self.action_open.setToolTip("Open")
+        self.action_open.setStatusTip("Open an image")
+        self.action_open.setShortcut("Ctrl+O")
+        self.action_open.setObjectName("actionOpen")
+
+        self.action_exit = QtWidgets.QAction(self)
+        self.action_exit.setText("Exit")
+        self.action_exit.setIconText("Exit")
+        self.action_exit.setToolTip("Exit")
+        self.action_exit.setStatusTip("Close all windows of an app")
+        self.action_exit.setShortcut("Ctrl+E")
+        self.action_exit.setObjectName("actionExit")
+
+        self.menu_file.addAction(self.action_open)
+        self.menu_file.addSeparator()
+        self.menu_file.addAction(self.action_exit)
+
+        self.menubar.addAction(self.menu_file.menuAction())
+
+        QtCore.QMetaObject.connectSlotsByName(self)
+
+        self.action_open.triggered.connect(self.browse_files)
+        self.action_exit.triggered.connect(self.close_event)
+
+    def browse_files(self):
+        file_name, _ = QFileDialog.getOpenFileName(self, "Open file...", "", "Image files (*.jpg *.jpeg *.bmp *.png *.tiff *.tif)")
+        if file_name:
+            self.new_image_window(file_name)
+        else:
+            self.label.setText("Choose an image")
+
+    def new_image_window(self, file_name):
+        window = ImageWindow(file_name)
+        self.windows.append(window)
+        window.show()
+
+    def close_event(self, event):
+        QApplication.closeAllWindows()
+
 
 if __name__ == "__main__":
+    import sys
     app = QtWidgets.QApplication(sys.argv)
-    window = ImageViewer()
-    window.show()
+    ui = UiMainWindow()
+    ui.show()
     sys.exit(app.exec_())
